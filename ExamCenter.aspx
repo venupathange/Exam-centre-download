e33b6560a53277f5e7f6fe6178db4fcd8742e2fc
venugopal <88709360+venupathange@users.noreply.github.com>
2024-12-26 11:38:41 +0530

    Add files via upload

---
+++ ExamCenter.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/Site.master" AutoEventWireup="true"
+ EnableEventValidation="false"CodeFile="ExamCenter.aspx.cs" Inherits="Examination_ExamCenter" 
+ ValidateRequest="false" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" Runat="Server">
+
+
+<TheoremWC:ToolBar ID="toolBar" runat="server" PageTitle="Configure Online Payment Paid Details">
+        <asp:UpdatePanel ID="update1" runat="server" UpdateMode="Conditional">
+            <ContentTemplate>
+                <table width="98%" style="margin: 0px 0px 0px 0px" border="0">
+                    <tr class="btn">
+                    <td align="left">
+                          <asp:LinkButton ID="btnPaymentDetails" OnClick="btnReport_Click" ValidationGroup="Report"  
+                              runat="server" Text="<img id='img' style='vertical-align:middle' src='../Images/pdf.gif' /> Report"  
+                 Style="vertical-align: middle; padding: 8px 16px; background-color: #4CAF50; color: white; border: none; border-radius: 4px; cursor: pointer;">
+                           </asp:LinkButton>  
+           </td>
+                    </tr>
+                  </table>
+               </ContentTemplate>
+               <Triggers>
+            <asp:PostBackTrigger ControlID="btnPaymentDetails" />
+             <%--<asp:PostBackTrigger ControlID="btnPaidChallan" />--%>
+            </Triggers>
+            </asp:UpdatePanel>
+          </TheoremWC:ToolBar> 
+         <TheoremWC:PageContent ID="pc" runat="server">
+        <table width="100%">
+            <tr>  
+           <td width="16%" align="left">  
+                 <td align="left" width="30%">
+                    <b>Exam Type</b>
+                    <br />
+                    <asp:DropDownList ID="ddlexamtype" runat="server" DataTextField="Name" DataValueField="Id" Width="60%">
+                    <asp:ListItem Text="Theory" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Practical" Value="0"></asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+           </td>
+           
+               <td align="left" width="30%">
+                    <b>Exam</b>
+                    <br />
+                    <asp:DropDownList ID="ddlexam" runat="server" DataTextField="Name" DataValueField="Id" Width="60%">
+                    <asp:ListItem Text="Annual Exam" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Supplimentary Exam" Value="0"></asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+           </tr>
+            </table>  
+            <Trigger>
+            <asp:AsyncPostBackTrigger ControlID="LinkButton1" EventName="Click" /> 
+            </Trigger>
+      </TheoremWC:PageContent>
+</asp:Content> 
+          
+    
+ 
---
+++ ExamCenter.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using Theorem.EOrg.Core.BAL;
+using Theorem.EOrg.Core.DEO;
+using Theorem.EOrg.StudentCore.BAL;
+using Theorem.EOrg.StudentCore.DEO;
+using Theorem.EOrg.Framework.Utilities;
+using DbLinq.Factory;
+using System.Data;
+using CrystalDecisions.Web;
+using CrystalDecisions.Shared;
+using CrystalDecisions.Enterprise;
+using CrystalDecisions.CrystalReports;
+using CrystalDecisions.CrystalReports.Engine;
+using BarCode;
+using System.IO;
+using System.Web.Services;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Script.Services;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using OfficeOpenXml.Utils;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.ConditionalFormatting;
+using OfficeOpenXml.VBA;
+using OfficeOpenXml.Table;
+using System.Drawing;
+using System.Web.Security;
+
+public partial class Examination_ExamCenter : System.Web.UI.Page
+{
+    UserManager objUserManager = null;
+    AdminManager objAdminManager = null;
+    StudentManager _studentMgr = null;
+    DataSet ds = null;
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+        }
+    }
+
+
+    protected void btnReport_Click(object sender, EventArgs e)
+    {
+        Examcentre();
+    }
+    private void Examcentre()
+    {
+        try
+        {
+            CreateStudentManagerInstance();
+            DataSet ds = _studentMgr.GetExamCenterallocated(Convert.ToInt32(this.ddlexamtype.SelectedValue),
+                                                               Convert.ToInt32(this.ddlexam.SelectedValue));
+
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                //Create the worksheet  
+
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Download");
+
+                ws.Cells["A1"].LoadFromDataTable(ds.Tables[0], true);
+
+                int columncount = ds.Tables[0].Columns.Count;
+
+                //Format the header for column 1-3  
+                using (ExcelRange rng = ws.Cells["A1:D1"])
+                {
+                    rng.Style.Font.Bold = true;
+                    rng.Style.Fill.PatternType = ExcelFillStyle.Solid;               //Set Pattern for the background to Solid  
+                    rng.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(79, 129, 189));  //Set color to dark blue  
+                    rng.Style.Font.Color.SetColor(Color.White);
+                }
+
+                //Example how to Format Column 1 as numeric  
+                using (ExcelRange col = ws.Cells[2, 1, 2 + ds.Tables[0].Rows.Count + 1, 1])
+                {
+                    //col.Style.Numberformat.Format = "#,##0.00";  
+                    col.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                }
+
+                for (int i = 0; i <= ds.Tables[0].Columns.Count; i++)
+                {
+                    //ws.Column(i).Width = 30;  
+                    ws.Column(i + 1).AutoFit();
+                    ws.Column(2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    ws.Column(4).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    ws.Column(6).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                }
+
+                //Write it back to the client  
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment; filename=Examcentre_" + DateTime.Now.ToString("yyyyMMdd") + ".xlsx");
+
+                Response.BinaryWrite(pck.GetAsByteArray());
+                Response.End();
+            }
+        }
+        catch (Exception ex)
+        {
+
+            throw ex;
+        }
+       // update1.Update();
+    }
+    private void CreateStudentManagerInstance()
+    {
+        if (_studentMgr == null)
+            _studentMgr = new StudentManager();
+    }
+
+
+}
